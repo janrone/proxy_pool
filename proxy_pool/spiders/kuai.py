5a49617d5ac0dcd385eb15204eedd7e2f269162f
yangzhiqiang <394502786@qq.com>
2017-11-17 23:08:41 +0800

    up kuai

--- proxy_pool/spiders/kuai.py
+++ proxy_pool/spiders/kuai.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 from scrapy.http import Request
 from proxy_pool.items import ProxyPoolItem
 
 class KuaiSpider(scrapy.Spider):
     name = 'kuai'
     allowed_domains = ['kuaidaili.com']
-    start_urls = ['http://www.kuaidaili.com/free/']
+    start_urls = ['http://www.kuaidaili.com/free/inha/1/',
+                  'http://www.kuaidaili.com/free/intr/1/']
 
     def parse(self, response):
+        typee, now_page = re.findall('com/free/(.*?)/(\d+)/', response.url)[0]
         iplist = response.xpath('//table[@class="table table-bordered table-striped"]/tbody/tr')
-        for x in iplist:
-            data = x.xpath('td/text()').extract()
-            item = ProxyPoolItem()
-            item['ip'] = data[0]
-            item['protocol'] = data[3]
-            item['port'] = data[1]
-            item['types'] = data[2]
-            yield item
+        if iplist:
+            for x in iplist:
+                data = x.xpath('td/text()').extract()
+                item = ProxyPoolItem()
+                item['ip'] = data[0]
+                item['protocol'] = data[3]
+                item['port'] = data[1]
+                item['types'] = data[2]
+                item['address'] = data[4]
+                yield item
+            next_page = int(now_page) + 1
+            next_url = 'http://www.kuaidaili.com/free/{}/{}/'.format(typee, next_page)
+            yield Request(next_url, self.parse)
+
+
