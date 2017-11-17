3ed8761524930b8264c8ea577f53b9d286925611
cjj0596 <cjj0596@175game.com>
2017-11-17 23:20:14 +0800

    change to country

--- proxy_pool/spiders/nianshao.py
+++ proxy_pool/spiders/nianshao.py
@@ -11,31 +11,24 @@ class NianshaoSpider(scrapy.Spider):
 
     def start_requests(self):
         pages=[]
-        http_url = 'http://www.nianshao.me/?stype=1'
-        https_url = 'http://www.nianshao.me/?stype=2'
-        http_count = re.findall('</font>/(\d+)</strong>',requests.get(http_url).text)[0]
-        https_count = re.findall('</font>/(\d+)</strong>',requests.get(https_url).text)[0]
-        for i in range(1,(int(http_count)+1)):
-            url = 'http://www.nianshao.me/?stype=1&page='+str(i)
-            page = scrapy.Request(url)
-            pages.append(page)
-
-        for i in range(1,(int(https_count)+1)):
-            url = 'http://www.nianshao.me/?stype=2&page='+str(i)
-            page = scrapy.Request(url)
-            pages.append(page)
-
+        
+        for i in range(1,3):
+            count = re.findall('</font>/(\d+)</strong>',requests.get('http://www.nianshao.me/?stype='+str(i)).text)[0]
+            for j in range(1,(int(count)+1)):
+                url = 'http://www.nianshao.me/?stype='+str(i)+'&page='+str(j)
+                page = scrapy.Request(url)
+                pages.append(page)
         return pages
 
     def parse(self, response):
         ips = re.findall('<td style="WIDTH:110PX">(\d+\.\d+\.\d+\.\d+)</td>', response.text)
         ports = re.findall('<td style="WIDTH:40PX">(\d+)</td>', response.text)
-        types = re.findall('<td style="WIDTH:135PX">([^<]+)</td>', response.text)
+        countrys = re.findall('<td style="WIDTH:135PX">([^<]+)</td>', response.text)
         protocols = re.findall('<td style="WIDTH:55PX">(HTTPS?)</td>', response.text)
-        for ip, port, _type, protocol in zip(ips, ports, types, protocols):
+        for ip, port, country, protocol in zip(ips, ports, countrys, protocols):
             yield ProxyPoolItem({
                 'ip': ip,
                 'protocol': protocol,
                 'port': port,
-                'types': _type
+                'country': country
             })
