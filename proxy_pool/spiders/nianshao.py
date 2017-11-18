8f8e89e34e80cafc506e73c6dd50306d70948e96
cjj0596 <cjj0596@175game.com>
2017-11-18 11:15:33 +0800

    update page_count

--- proxy_pool/spiders/nianshao.py
+++ proxy_pool/spiders/nianshao.py
@@ -6,23 +6,28 @@ from proxy_pool.items import ProxyPoolItem
 class NianshaoSpider(scrapy.Spider):
     name = 'nianshao'
     allowed_domains = ['nianshao.me']
-
-    def start_requests(self):
-        for i in range(1,3):
-            for j in range(1,161):
-                url = 'http://www.nianshao.me/?stype={protocol_id}&page={page}'.format(protocol_id=i,page=j)
-                yield scrapy.Request(url)
+    start_urls = ["http://www.nianshao.me/?stype=1&page=1", 
+                  "http://www.nianshao.me/?stype=2&page=1"]
 
     def parse(self, response):
-        ips = re.findall('<td style="WIDTH:110PX">(\d+\.\d+\.\d+\.\d+)</td>', response.text)
-        ports = re.findall('<td style="WIDTH:40PX">(\d+)</td>', response.text)
-        addresses = re.findall('<td style="WIDTH:135PX">([^<]+)</td>', response.text)
-        protocols = re.findall('<td style="WIDTH:55PX">(HTTPS?)</td>', response.text)
-        for ip, port, address, protocol in zip(ips, ports, addresses, protocols):
-            yield ProxyPoolItem({
-                'ip': ip,
-                'protocol': protocol,
-                'port': port,
-                'address': address,
-                'website': 'www.nianshao.me'
-            })
+        try:
+            now_page, page_count = re.findall('<strong><font color="#49afcd">(\d+)</font>/(\d+)</strong>', response.text)[0]
+            ips = re.findall('<td style="WIDTH:110PX">(\d+\.\d+\.\d+\.\d+)</td>', response.text)
+            ports = re.findall('<td style="WIDTH:40PX">(\d+)</td>', response.text)
+            types = re.findall(u"<td style=\"WIDTH:55PX\">([\u4e00-\u9fa5]+)</td>",response.text)
+            addresses = re.findall(u"<td style=\"WIDTH:135PX\">([[\u4e00-\u9fa5]+)</td>", response.text)
+            protocols = re.findall('<td style="WIDTH:55PX">(HTTPS?)</td>', response.text)
+            for ip, port, typ, address, protocol in zip(ips, ports, types, addresses, protocols):
+                yield ProxyPoolItem({
+                    'ip': ip,
+                    'protocol': protocol,
+                    'types': typ,
+                    'port': port,
+                    'address': address,
+                    'website': 'www.nianshao.me'
+                })
+            _next = int(now_page) + 1 
+            _next_url = re.sub('page=(\d+)', "page="+str(_next), response.url)
+            yield scrapy.Request(_next_url, self.parse)
+        except Exception as e:
+            pass
