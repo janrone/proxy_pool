309262526b53796e1de43f66650bf9447ac2b079
janrone <janrone@qq.com>
2017-11-21 14:03:25 +0800

    update goubanjia.py

--- proxy_pool/spiders/goubanjia.py
+++ proxy_pool/spiders/goubanjia.py
@@ -16,8 +16,8 @@ class GoubanjiaSpider(scrapy.Spider):
                item = ProxyPoolItem()
                info = x.xpath('td')
                ipport = x.xpath('td[1]//*[name(.)!="p"]/text()').extract()
-               address =  x.xpath('td[4]//*[name(.)="a"]/text()').extract()
-               item['ip'] =  "".join(ipport[:-1])
+               address = x.xpath('td[4]//*[name(.)="a"]/text()').extract()
+               item['ip'] = "".join(ipport[:-1])
                item['port'] = ipport[-1]
                item['protocol'] = info[2].xpath('string(.)').extract()[0]
                item['types'] = info[1].xpath('string(.)').extract()[0] 
